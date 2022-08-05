54acaaab84b7b0fea8e8224ab3b7003db11b25b6
Timur Zaripov <timur.zaripov.05@gmail.com>
2022-08-05 15:31:13 +0300

    task1 first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Integer> list = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        ArrayList<Integer>arrayList = new ArrayList<>(list);
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i) % 2 != 0 || arrayList.get(i) <= 0){
+                arrayList.remove(i);
+                i--;
+            }
+        }
+        arrayList.sort(Integer::compare);
+        System.out.println(arrayList);
     }
 }
